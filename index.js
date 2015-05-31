3dcf2563e2b98bc1ceca31efb63389d0728ead92
Alec Hollingsworth <alec@mailc.net>
2015-05-31 08:35:49 -0700

    add skipping to Ranges, add .by(skip) method

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 'use strict';
 
-function Range(from, upto) {
+function Range(from, upto, skip) {
   this.range = [];
 
   if (from !== undefined) {
     if (upto !== undefined) {
-      this.range = Range.inclusive(from, upto).range;
+      if (skip !== undefined) {
+        this.range = Range.inclusive(from, upto, skip).range;
+      } else {
+        this.range = Range.inclusive(from, upto).range;
+      }
     } else {
       this.range = Range.upto(from).range;
     }
   }
 }
 
-Range.inclusive = function(from, upto) {
+Range.inclusive = function(from, upto, skip) {
   var r = new Range();
-  for (; upto >= from; upto--) {
+  skip = skip || 1;
+  upto -= (upto - from) % skip; // Adjust upper bound down to fit
+  for (; upto >= from; upto -= skip) {
     r.range.unshift(upto);
   }
   return r;
 };
 
-Range.exclusive = function(from, upto) {
-  var r = new Range();
-  for (upto--; upto >= from; upto--) {
-    r.range.unshift(upto);
-  }
-  return r;
+Range.exclusive = function(from, upto, skip) {
+  return Range.inclusive(from, upto - 1, skip);
 };
 
 Range.upto = function(upto) {
@@ -34,6 +36,10 @@ Range.upto = function(upto) {
 
 Range.below = function(below) {
   return Range.exclusive(1, below);
+};
+
+Range.prototype.by = function(skip) {
+  return new Range(this.range[0], this.range[this.range.length - 1], skip);
 };
 
 Range.prototype.toString = function() {
